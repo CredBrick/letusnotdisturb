fa4fcab744ee56438dc0581b97f82927a5f751d8
ishanwadhwani <ishan.209303191@muj.manipal.edu>
2023-08-21 14:49:25 +0530

    news page added

---
+++ constants/data.js
@@ -0,0 +1,21 @@
+const instapostdata = [
+    { id : 1, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1639372095078-V2MB6U7P5HFMK64DSOGT/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CXaNcMSPvJ9/"},
+    { id : 2, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1639372095078-1WRU8BKVS35RDXM2EY19/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CXaNYyDPQP9/"},
+    { id : 3, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1639372095078-V2MB6U7P5HFMK64DSOGT/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CXaNUBqvTTc/"},
+    { id : 4, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1636872475966-C2VIHSU6OICXYK53K2GJ/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CWPtOQlPzxq/"},
+    { id : 5, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1636872475966-0WCT8VVFQPHTGAT4WN12/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CWPtK3LP1lM/"},
+    { id : 6, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1636872475967-O9FDZAS19WV2JUROIVR9/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CWPtGBlP_Ve/"},
+    { id : 7, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1634793674354-YLKIB652BL0BXS3LGFZU/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CVRySOusFdl/"},
+    { id : 8, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1634793674354-DFWXSVZT31OQ8ZKFRUR2/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CVRx-GXMO4Z/"},
+    { id : 9, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1634793674354-U8G9HOLX7YHEO221OOV4/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CVRx5ERMXgU/"},
+    { id : 10, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1643882681547-5QJ09SMGMRVOU00G3BMG/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CUjgppmsVC3/"},
+    { id : 11, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1633240085838-W3IQNJ4KIURHXS839A4O/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CUjgaNwsuHF/"},
+    { id : 12, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1639176359649-NJRCCU4CLHQ233CNC0U3/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CUjgWFLMB7Q/"},
+    { id : 13, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1632207947744-RNVDEQ080FRL5OCNUG2C/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CT1CehhJ_nu/"},
+    { id : 14, imgurl: "https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1632207947744-6FA4EQURKGGI15WBZBWW/image-asset.jpeg", instaurl: "https://www.instagram.com/p/CT1CaTIpEZG/"}
+]
+
+export {
+    
+    instapostdata,
+}
