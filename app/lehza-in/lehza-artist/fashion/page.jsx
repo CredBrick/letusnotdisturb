188205b4bb4da5060e3217502b8bcd4569c476af
ishanwadhwani <ishan.209303191@muj.manipal.edu>
2023-09-07 00:57:41 +0530

    added more pages like cinema, fashion etc

--- app/lehza-in/lehza-artist/fashion/page.jsx
+++ app/lehza-in/lehza-artist/fashion/page.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='text-white'>
+      <div className='mt-10 text-center'>
+        <h2 className='px-[150px] text-3xl mb-10'>
+          "Don't be into trends. Don't make fashion own you, 
+          but you decide what you are, what you want to express 
+          by the way you dress and the way to live." 
+        </h2>
+        <a 
+          href='https://en.wikipedia.org/wiki/Gianni_Versace'
+          className='italic underline text-lg mb-10'> 
+          — Gianni Versace
+        </a>
+        <h1 className='md:text-5xl lg:text-7xl px-[300px] mt-10'>
+          Lehza.in x Fashion
+        </h1>
+      </div>
+    </div>
   )
 }
 
