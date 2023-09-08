45fa6df89518a0e9386595ab26cd3dbc86deef1e
ishanwadhwani <ishan.209303191@muj.manipal.edu>
2023-09-08 23:26:14 +0530

    new pages added

--- app/lehza-in/lehza-artist/author/page.jsx
+++ app/lehza-in/lehza-artist/author/page.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='text-white'>
+      <div className='mt-10 text-center'>
+        <h2 className='px-[150px] text-3xl mb-10'>
+          Authors must spend months, years making fantasy believable in a 
+          single work while reality runs rampant and complete chaos elsewhere.
+        </h2>
+        <p className='italic underline text-lg mb-10'> 
+            — Don Roff
+        </p>
+        <h1 className='md:text-5xl lg:text-7xl px-[300px] mt-10'>
+          Lehza.in x Author
+        </h1>
+      </div>
+    </div>
   )
 }
 
--- app/lehza-in/lehza-artist/social-activists/page.jsx
+++ app/lehza-in/lehza-artist/social-activists/page.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='text-white'>
+      <div className='mt-10 text-center'>
+        <h2 className='px-[150px] text-3xl mb-10'>
+          It is not enough to be compassionate.
+          <br /> 
+          You must act.
+        </h2>
+        <a 
+          href='https://en.wikipedia.org/wiki/Dalai_Lama'
+          className='italic underline text-lg mb-10'> 
+            — Dalai Lama
+        </a>
+        <h1 className='md:text-5xl lg:text-7xl px-[300px] mt-10'>
+          Lehza.in x Social Activists
+        </h1>
+      </div>
+    </div>
   )
 }
 
