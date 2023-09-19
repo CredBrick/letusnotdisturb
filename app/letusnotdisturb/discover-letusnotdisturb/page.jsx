b52ee1f423a335cf38e75616e81e7d7b7d1ee69d
ishanwadhwani <ishan.209303191@muj.manipal.edu>
2023-09-19 23:50:14 +0530

    some changes are made

--- app/letusnotdisturb/discover-letusnotdisturb/page.jsx
+++ app/letusnotdisturb/discover-letusnotdisturb/page.jsx
@@ -5,19 +5,27 @@ const page = () => {
     <div className=''>
       <div className='font-extralight'>
         <img
-          className="object-cover md:w-full min-w-[350px] md:h-[450px] h-[400px]"
+          className="relative object-cover md:w-full min-w-[350px] md:h-[450px] h-[400px]"
           src="https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1624635185627-9RRCPSQ767JTAJXZUVNO/atibaia-3381711_1920.jpg"
+          alt="could not load the image"
         />
-        <div className="flex items-center justify-center text-center text-black leading-[80px] ">
-            <p className="absolute lg:text-3xl md:text-2xl text-xl md:top-[100px] top-[130px] px-10 md:px-[130px] md:leading-10 lg:leading-[50px]">
+        <div className="flex flex-col items-center justify-center text-center text-black leading-[80px] ">
+            <p className="absolute lg:text-3xl md:text-2xl text-xl top-[130px] md:top-[150px] lg:top-[180px] px-10 md:px-[130px] lg:px-[230px] md:leading-10 lg:leading-[50px]">
               Comforting the disturbed and disturbing the comfortable! Our intention is to do exactly what we've been doing, TALK. 
               We went, We saw, We heard, We forgot. But we'll discuss, because nothing escapes our experienced eyes. 
               Let us just be what we are, we have no intention to disturb your sweet lives. 
             </p>
-            <p className='absolute mb-[180px] lg:mb-[140px] text-3xl'>
+            <p className='absolute mb-[180px] lg:mb-[170px] text-2xl md:text-3xl'>
               So, LET US NOT DISTURB 
             </p>
         </div>
+      </div>
+
+      <div>
+        <img 
+          src="https://images.squarespace-cdn.com/content/v1/60bcdce3539722187fb73258/1628010941889-LK6F0K3X8MDB5IRXA3NG/Stationary.jpg" 
+          alt="could not load the image"
+          className='w-full h-auto px-10' />
       </div>
     </div>
   )
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,143 +7,144 @@ import { AiFillInstagram } from "react-icons/ai"
 const Footer = () => {
     return (
         <footer className="background text-white pt-8 pb-3">
-                <div className="ml-6 grid grid-rows-1 md:grid-cols-4 justify-center items-center gap-[35px] md:gap-[85px]">
+            <div className="ml-6 grid grid-rows-1 md:grid-cols-4 justify-center items-center text-center gap-[35px] md:gap-[85px]">
 
-                    {/* Learn More START */}
-                    <div className="flex flex-col gap-3">
-                        <div className="font-oswald font-medium uppercase">
-                            Learn More
-                        </div>
-                        <Link
-                            href="/"
-                            className="font-oswald font-medium uppercase text-sm cursor-pointer underline">
-                            Terms & Condition
-                        </Link>
-                        <Link 
-                            href="/"
-                            className="font-oswald font-medium uppercase text-sm cursor-pointer underline">
-                            Privacy Policy
-                        </Link>
-                        <Link
-                            href="/"
-                            className="font-oswald font-medium uppercase text-sm cursor-pointer underline">
-                            About Us
-                        </Link>
-                        <Link
-                            href="/" 
-                            className="font-oswald font-medium uppercase text-sm cursor-pointer underline">
-                            Contact Us
-                        </Link>
+                {/* Learn More START */}
+                <div className="flex flex-col gap-3">
+                    <div className="font-oswald font-medium uppercase">
+                        Learn More
                     </div>
-                    {/* Learn More END */}
-
-
-                    {/* Discover About START */}
-                    <div className="flex md:flex-row flex-col gap-[50px] md:mb-10">
-                        <div className="flex flex-col gap-5">
-                            <div className="font-oswald font-medium uppercase text-md">
-                                Discover About
-                            </div>
-                            <Link
-                                href='https://letusnotdisturb.com/lehza-in/' 
-                                className="text-md text-white hover:text-white cursor-pointer underline">
-                                Lehaz.in
-                            </Link>
-                            <Link
-                                href='https://www.letusnotdisturb.com/discover-let-us-not-disturb' 
-                                className="text-md text-white hover:text-white cursor-pointer underline">
-                                LetUsNotDisturb
-                            </Link>
-                        </div>
-                    </div>
-                    {/* Discover About END */}
-
-
-                    {/* Social Media START */}
-                    <div className='flex flex-col md:mb-[80px] lg:mb-[90px]'>
-                        <div className='font-oswald font-medium uppercase text-md'>
-                            Social Media
-                        </div>
-                    
-                        <div className='flex justify-between flex-row gap-2 py-3'>
-                            <div
-                                onClick={() => 
-                                    window.open("https://www.instagram.com/lehza.in/", "_blank")
-                                    } 
-                                className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                                <AiFillInstagram size={20} />
-                            </div>
-
-                            <div
-                                onClick={() => 
-                                    window.open("https://www.facebook.com/lehza.in/", "_blank")
-                                    }
-                                className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center 
-                                justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                                <FaFacebookF size={20} />
-                            </div>
-
-                            <div
-                                onClick={() => 
-                                    window.open("https://www.linkedin.com/company/lehza-in/", "_blank")
-                                    }
-                                className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center 
-                                justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                                <FaLinkedinIn size={20} />
-                            </div>
-                            
-                            <div 
-                                onClick={() => 
-                                    window.open("https://www.youtube.com/channel/UC3jvS6mi7udT5KLLqq83miQ", "_blank")
-                                    }
-                                className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                                <FaYoutube size={20} />
-                            </div>
-
-                            <div 
-                                onClick={() => 
-                                    window.open("https://twitter.com/letusnotdisturb", "_blank")
-                                    }
-                                className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                                <FaTwitter size={20} />
-                            </div>
-
-                            <div
-                                onClick={() => 
-                                    window.open("https://www.instagram.com/letusnotdisturb/", "_blank")
-                                    } 
-                                className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                                <AiFillInstagram size={20} />
-                            </div>
-
-                            <div
-                                onClick={() => 
-                                    window.open("https://www.linkedin.com/in/let-us-not-disturb-814454106/", "_blank")
-                                    }
-                                className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center 
-                                justify-center text-black hover:bg-white/[0.5] cursor-pointer">
-                                <FaLinkedinIn size={20} />
-                            </div>
-
-                        </div>
-                    </div>
-                    {/* Social Media END */}
-                    
-
-                    {/* Created By START */}
-                    <div className="flex flex-col gap-2 justify-center md:justify-start md:mb-[100px] lg:mb-[130px]">
-                        <div>Created By</div>
-                        <div>
-                        <Link
-                            href='https://askashirvad.com/'>
-                            <div className="font-oswald text-md cursor-pointer underline">
-                                AskAshirvad
-                            </div>
-                        </Link>
-                        </div>
-                    </div>
-                    {/* Created By END */}
+                    <Link
+                        href="/"
+                        className="font-oswald font-medium uppercase text-sm cursor-pointer underline">
+                        Terms & Condition
+                    </Link>
+                    <Link 
+                        href="/"
+                        className="font-oswald font-medium uppercase text-sm cursor-pointer underline">
+                        Privacy Policy
+                    </Link>
+                    <Link
+                        href="/"
+                        className="font-oswald font-medium uppercase text-sm cursor-pointer underline">
+                        About Us
+                    </Link>
+                    <Link
+                        href="/" 
+                        className="font-oswald font-medium uppercase text-sm cursor-pointer underline">
+                        Contact Us
+                    </Link>
                 </div>
+                {/* Learn More END */}
+
+
+                {/* Discover About START */}
+                <div className="flex md:flex-row flex-col gap-[50px] md:mb-10">
+                    <div className="flex flex-col gap-5">
+                        <div className="font-oswald font-medium uppercase text-md">
+                            Discover About
+                        </div>
+                        <Link
+                            href='https://letusnotdisturb.com/lehza-in/' 
+                            className="text-md text-white hover:text-white cursor-pointer underline">
+                            Lehaz.in
+                        </Link>
+                        <Link
+                            href='https://www.letusnotdisturb.com/discover-let-us-not-disturb' 
+                            className="text-md text-white hover:text-white cursor-pointer underline">
+                            LetUsNotDisturb
+                        </Link>
+                    </div>
+                </div>
+                {/* Discover About END */}
+
+
+                {/* Social Media START */}
+                <div className='flex flex-col md:mb-[80px] lg:mb-[90px]'>
+                    <div className='font-oswald font-medium uppercase text-md'>
+                        Social Media
+                    </div>
+                
+                    <div className='flex justify-between flex-row gap-2 py-3'>
+                        <div
+                            onClick={() => 
+                                window.open("https://www.instagram.com/lehza.in/", "_blank")
+                                } 
+                            className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                            <AiFillInstagram size={20} />
+                        </div>
+
+                        <div
+                            onClick={() => 
+                                window.open("https://www.facebook.com/lehza.in/", "_blank")
+                                }
+                            className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center 
+                            justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                            <FaFacebookF size={20} />
+                        </div>
+
+                        <div
+                            onClick={() => 
+                                window.open("https://www.linkedin.com/company/lehza-in/", "_blank")
+                                }
+                            className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center 
+                            justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                            <FaLinkedinIn size={20} />
+                        </div>
+                        
+                        <div 
+                            onClick={() => 
+                                window.open("https://www.youtube.com/channel/UC3jvS6mi7udT5KLLqq83miQ", "_blank")
+                                }
+                            className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                            <FaYoutube size={20} />
+                        </div>
+
+                        <div 
+                            onClick={() => 
+                                window.open("https://twitter.com/letusnotdisturb", "_blank")
+                                }
+                            className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                            <FaTwitter size={20} />
+                        </div>
+
+                        <div
+                            onClick={() => 
+                                window.open("https://www.instagram.com/letusnotdisturb/", "_blank")
+                                } 
+                            className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                            <AiFillInstagram size={20} />
+                        </div>
+
+                        <div
+                            onClick={() => 
+                                window.open("https://www.linkedin.com/in/let-us-not-disturb-814454106/", "_blank")
+                                }
+                            className="w-7 h-7 rounded-full bg-white/[0.25] flex items-center 
+                            justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                            <FaLinkedinIn size={20} />
+                        </div>
+
+                    </div>
+                </div>
+                {/* Social Media END */}
+                
+
+                {/* Created By START */}
+                <div className="flex flex-col gap-2 justify-center md:justify-start md:mb-[100px] lg:mb-[130px]">
+                    <div>Created By</div>
+                    <div>
+                    <Link
+                        href='https://askashirvad.com/'>
+                        <div className="font-oswald text-md cursor-pointer underline">
+                            AskAshirvad
+                        </div>
+                    </Link>
+                    </div>
+                </div>
+                {/* Created By END */}
+
+            </div>
         </footer>
     );
 };
